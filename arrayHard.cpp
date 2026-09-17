0cf9980f3e195061183f41ceb13c23809ba4a69b
Gaurav Yadav <gauravyadavpro63@gmail.com>
2026-09-17 19:58:12 +0530

    array-hard

--- arrayHard.cpp
+++ arrayHard.cpp
@@ -247,4 +247,242 @@
     //         mpp[XOR]++;
     //     }
  //     return cnt;
-}
+// }
+
+
+// #merge overlaping subintervals
+
+
+    // vector<vector<int>> merge(vector<vector<int>>& interval) {
+
+    //         int n=interval.size();
+    // vector<vector<int>>ans;
+    // sort(interval.begin(),interval.end());
+    // ans.push_back(interval[0]);
+    // for(int i=1;i<n;i++){
+    //     int start=interval[i][0];
+    //     int end=interval[i][1];
+    //     if(start<=ans.back()[1]){
+    //         ans.back()[1]=max(ans.back()[1],end);
+    //     }
+    //     else{
+    //         ans.push_back({start,end});
+    //     }
+
+    // }
+    // return ans;
+    // }
+
+
+
+    // #merge two sorted array
+        // brute tc=O(n+m)+O(n+m) sc=O(n+m);
+    // int n=4;
+    // int m=5;
+    // int arr3[n+m];
+    // int left=0;
+    // int right=0;
+    // int index=0;
+    // while(left<n&&right<m){
+    //     if(arr1[left]<=arr2[right]){
+    //         arr3[index]=arr1[left];
+    //         left++;index++;
+    //     }
+    //     else{
+    //         arr3[index]=arr2[right];
+    //         right++,index++;
+    //     }
+    // }
+    // while(left<n){
+    //     arr3[index++]=arr1[left++];
+    // }
+    // while(right<m){
+    //     arr3[index++]=arr2[right++];
+    // }
+    
+    // for(int i=0;i<n+m;i++){
+    //     if(i<n) arr1[i]=arr3[i];
+    //     else arr2[i-n]=arr3[i];
+    // }
+
+    // optimal tc=O(min(n,m)) +nlogn+mlogm sc=O(1);
+    // int n=4;
+    // int m=5;
+    // int left=n-1;
+    // int right=0;
+    // while(left>=0&&right<m){
+    //     if(arr1[left]>arr2[right]){
+    //         swap(arr1[left],arr2[right]);
+    //         left--,right++;
+    //     }
+    //     else{
+    //         break;
+    //     }
+
+    // }
+    // sort(arr1,arr1+n);
+    // sort(arr2,arr2+m);
+
+    // #find missing and repeating number
+    // brute--->n*n
+    // better--->hashmap
+    // optimal--->
+       // optimal 1 tc=O(n) sc=O(1);
+    // long long n=arr.size();
+    // // s-Sn=x-y     //repeating-missing
+    // // s2-S2n=Xsq-Ysq
+    // long long Sn=(n*(n+1))/2;
+    // long long S2n=(n*(n+1)*(2*n+1))/6;
+    // long long s=0,s2=0;
+    // for(int i=0;i<n;i++){
+    //     s+=arr[i];
+    //     s2+=(long long)arr[i]*(long long)arr[i];
+    // }
+    // long long val1=s-Sn;//x-y
+    // long long val2=s2-S2n;
+    // val2=val2/val1; //x+y
+    // long long x=(val1+val2)/2;
+    // long long y=x-val1;
+    // return {(int)x,(int)y};
+
+
+// # count no of pairs in which first element is greater than second element (count inversions);
+// brute --->n*n 
+// optimal solution--->tc=O(nlogn ) sc=O(n)
+// int merging(vector<int> &arr,int low,int mid,int high){
+//     int cnt=0;
+//      vector<int> temp;
+//     int left=low;
+//     int right=mid+1;
+//     while(left<=mid&&right<=high){
+//        if(arr[left]<=arr[right]){
+//         temp.push_back(arr[left]);
+//         left++;
+//        }
+//        else{
+//         cnt+=mid-left+1;
+//         temp.push_back(arr[right]);
+//         right++;
+//        }
+//     }
+//     while(left<=mid){
+//         temp.push_back(arr[left]);
+//         left++;
+//     }
+//     while(right<=high){
+//         temp.push_back(arr[right]);
+//         right++;
+//     }
+//     for(int i=low;i<=high;i++){
+//         arr[i]=temp[i-low];
+//     }
+//     return cnt;
+// }
+// int merge_sort(vector<int> &arr,int low,int high){
+//     int cnt=0;
+//     if(low>=high) return cnt;
+//     int mid=(low+high)/2;
+//     cnt+=merge_sort(arr,low,mid);
+//     cnt+=merge_sort(arr,mid+1,high);
+//     cnt+=merging(arr,low,mid,high);
+//     return cnt;
+// }
+// int count_inversions2(vector<int> &arr){
+//     int n=arr.size();
+//     return merge_sort(arr,0,n-1);
+// }
+// int main(){
+//     vector<int>arr={5,3,2,4,1};
+//     // cout<<count_inversions(arr);
+//     cout<<count_inversions2(arr);
+
+// }
+
+
+
+// # no of pairs in which left element is greater than right by 2 times (reverse pair)
+// brute --->n*n
+// optimal solution---> we use anther function becase useing it like before will disturb the sorting alogo due to 2*n codition 
+
+// optimal solution sc=O(n) tc= logn*(n+n)=2nlogn
+// void merging(vector<int>&arr,int low,int mid,int high){
+//    int left=low;
+//    int right=mid+1;
+//    vector<int>temp;
+//    while(left<=mid&&right<=high){
+//     if(arr[left]<=arr[right]) {
+//         temp.push_back(arr[left]);
+//         left++;
+//     }
+//     else{
+//         temp.push_back(arr[right]);
+//         right++;
+//     }
+//    }
+//     while(left<=mid){
+//         temp.push_back(arr[left]);
+//         left++;
+//     }
+//     while(right<=high){
+//         temp.push_back(arr[right]);
+//         right++;
+//     }
+//     for(int i=low;i<=high;i++){
+//         arr[i]=temp[i-low];
+//     }
+// }
+// int count_pairs(vector<int>&arr,int low,int mid,int high){
+//     int cnt=0;
+//     int right=mid+1;
+//     for(int i=low;i<=mid;i++){
+//         while(right<=high&&arr[i]>(long long)2*arr[right]){
+//             right++;
+//         }
+//         cnt+=right-(mid+1);
+//     }
+//     return cnt;
+// }
+// int merge_sort(vector<int> &arr,int low,int high){
+//     int cnt=0;
+//     int mid=(low+high)/2;
+//     if(low>=high) return cnt;
+//     cnt+=merge_sort(arr,low,mid);
+//     cnt+=merge_sort(arr,mid+1,high);
+//     cnt+=count_pairs(arr,low,mid,high);
+//     merging(arr,low,mid,high);
+//     return cnt;
+// }
+// int reverse_pairs(vector<int>& arr){
+//     int n=arr.size();
+//  return merge_sort(arr,0,n-1);
+// }
+
+
+
+// #maximum product subarray
+// brute --->n*n
+// optimal 
+    // optimal solutin  sc=O(1) tc=O(n):
+//     int n=arr.size();  // age-piche algoritm
+//     int prefix=1;
+//     int suffix=1;
+//     int maxi=INT_MIN;
+//     for(int i=0;i<n;i++){
+//         prefix*=arr[i];
+//         suffix*=arr[n-i-1];
+//         maxi=max(maxi,max(prefix,suffix));
+//          if(prefix==0) prefix=1;
+//         if(suffix==0) suffix=1;
+
+//     }
+//     return maxi;
+
+// }
+
+
+
+#######################################################################################
+                               GOD GIVE ME EVERYTHING
+#######################################################################################
+
+
