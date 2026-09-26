4a5fa4d4b61807387be09d7b56f5e68a8e99c5b2
Gaurav Yadav <gauravyadavpro63@gmail.com>
2026-09-27 00:34:28 +0530

    StrigEasy

--- stringEasy.cpp
+++ stringEasy.cpp
@@ -96,3 +96,73 @@
 // return ans;
 
 // }
+
+
+
+#IS STRING  ISOMORPHIC
+
+
+// bool is_string_isomorphic(string s,string d){
+//     unordered_map<char,char>mp1;
+//     unordered_map<char,char>mp2;
+//     if(s.size()!=d.size()){
+//         return false;
+//     }
+//     for(int i=0;i<s.size();i++){
+//         char a=s[i];
+//         char b=d[i];
+//          if(mp1.count(a)&&mp1[a]!=b){
+//             return false;
+//          }
+//          if(mp2.count(b)&&mp2[b]!=a){ 
+//             return false;
+//          }
+//          mp1[a]=b;
+//          mp2[b]=a;
+//     }
+//     return true;
+// }
+
+
+
+#ROTATE STRING
+
+
+//     if(s.size()!=goal.size()) return false;
+    // for(int i=0;i<s.size();i++){
+    //     if(s==goal){
+    //         return true;
+    //     }
+    //     char k=s[0];
+    //     s.erase(0,1);
+    //     s+=k;
+
+    // }
+    // return false;
+    // optimal solution
+
+
+
+    #IS STRING ANAGRAM
+
+
+
+//     bool is_string_anagram(string s,string k){
+    // brute solution nlogn;
+    // if(s.length()!=k.length()) return false;
+    // sort(s.begin(),s.end());
+    // sort(k.begin(),k.end());
+    // if(s==k) return true;
+    // else return false;
+    // optimal solution
+//     if(s.length()!=k.length()) return false;
+//     int hashmap[26]={0};
+//     for(int i=0;i<s.length();i++){
+//         hashmap[s[i]-'a']++;
+//         hashmap[k[i]-'a']--;
+//     }
+//     for(int i=0;i<26;i++){
+//         if(hashmap[i]!=0) return false;
+//     }
+//     return true;
+// }
